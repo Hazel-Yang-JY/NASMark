c6bfb251b341fe5891bbd95ffe128534978e46f8
Hazel <jyy20205@gmail.com>
2026-08-16 18:33:32 +1000

    Add imports and define __all__ in __init__.py

--- nasmark/__init__.py
+++ nasmark/__init__.py
@@ -1,1 +1,11 @@
+from .blocks import MobileInvertedResidual, WatermarkModule
+from .supernet import SPOSMobileNetSubnet, SPOSMobileNetSupernet
+
+__all__ = [
+    "MobileInvertedResidual",
+    "WatermarkModule",
+    "SPOSMobileNetSupernet",
+    "SPOSMobileNetSubnet",
+]
+
 
