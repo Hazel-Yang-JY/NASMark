fd9f2acf89f60b4b259d91080362c8e4b8ff57e7
Hazel <jyy20205@gmail.com>
2026-08-20 15:36:31 +1000

    Delete nasmark/models/__init__.py

--- nasmark/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""NASMark: watermark-aware one-shot neural architecture search."""
-
-from .models.supernet import SPOSMobileNetSupernet, SPOSMobileNetSubnet
-
-__all__ = ["SPOSMobileNetSupernet", "SPOSMobileNetSubnet"]
-
